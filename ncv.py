12e380c845cee59906bba8e07288aab8eb149775
SpencerCooley07 <spencerjcooley07@gmail.com>
2025-07-15 09:04:25 +1000

    Added Parameters + Switched to AdamW With Weight Decay

--- ncv.py
+++ ncv.py
@@ -16,23 +16,27 @@ INNER_K = 4
 PARAMETER_ITER = 1
 PARAMETER_GRID = {
     "LR": reciprocal(1e-5, 1e-2),
-    "BATCH_SIZE": [8, 64],
+    "BATCH_SIZE": [32, 64, 128],
     "EPOCHS": [1],
-    "PATIENCE": [10, 15, 20, 25]
+    "PATIENCE": [5, 10, 15],
+    "DROPOUT": [0.3, 0.5, 0.7],
+    "WEIGHT_DECAY": reciprocal(1e-4, 1e-2)
 }
 
-# PARAMETER_ITER = 10
+# PARAMETER_ITER = 20
 # PARAMETER_GRID = {
 #     "LR": reciprocal(1e-5, 1e-2),
-#     "BATCH_SIZE": [8, 16, 32, 64],
+#     "BATCH_SIZE": [16, 32, 64, 128],
 #     "EPOCHS": [50, 100, 150, 200],
-#     "PATIENCE": [10, 15, 20, 25]
+#     "PATIENCE": [5, 10, 15],
+#     "DROPOUT": [0.3, 0.5, 0.7],
+#     "WEIGHT_DECAY": reciprocal(1e-4, 1e-2)
 # }
 
 PATIENT_DATA = PatientDataset("data")
 
-kf_outer = KFold(n_splits=OUTER_K, shuffle=True, random_state=1)
 results = []
+kf_outer = KFold(n_splits=OUTER_K, shuffle=True, random_state=1)
 for i_outer_fold, (train_idx, test_idx) in enumerate(kf_outer.split(range(len(PATIENT_DATA)))):
     print(f"[OUTER {i_outer_fold+1}/{OUTER_K}]")
 
@@ -47,8 +51,8 @@ for i_outer_fold, (train_idx, test_idx) in enumerate(kf_outer.split(range(len(PA
     best_model_config = None
 
     parameter_configs = list(ParameterSampler(PARAMETER_GRID, n_iter=PARAMETER_ITER, random_state=i_outer_fold))
-    for i_parameter_config, parameter_config in enumerate(parameter_configs):
-        print(f"    [CONFIG {i_parameter_config+1}/{PARAMETER_ITER}] | [LR: {parameter_config['LR']:.4f} | B: {parameter_config['BATCH_SIZE']:02d} | E: {parameter_config['EPOCHS']:03d}]")
+    for i_parameter_config, config in enumerate(parameter_configs):
+        print(f"    [CONFIG {i_parameter_config+1}/{PARAMETER_ITER}] | [LR: {config['LR']:.4f} | BS: {config['BATCH_SIZE']:02d} | E: {config['EPOCHS']:03d} | P: {config['PATIENCE']:02d} | D: {config['DROPOUT']} | WD: {config['WEIGHT_DECAY']:.4f}]")
         fold_losses = []
 
         for i_inner_fold, (inner_train_idx, inner_val_idx) in enumerate(kf_inner.split(outer_train_subjects)):
@@ -58,13 +62,13 @@ for i_outer_fold, (train_idx, test_idx) in enumerate(kf_outer.split(range(len(PA
             inner_train_subjects = [outer_train_subjects[i] for i in inner_train_idx]
             inner_val_subjects = [outer_train_subjects[i] for i in inner_val_idx]
 
-            inner_train_loader = DataLoader(SegmentDataset(inner_train_subjects), batch_size=parameter_config["BATCH_SIZE"], shuffle=True)
-            inner_val_loader = DataLoader(SegmentDataset(inner_val_subjects), batch_size=parameter_config["BATCH_SIZE"])
+            inner_train_loader = DataLoader(SegmentDataset(inner_train_subjects), batch_size=config["BATCH_SIZE"], shuffle=True)
+            inner_val_loader = DataLoader(SegmentDataset(inner_val_subjects), batch_size=config["BATCH_SIZE"])
 
             model = OSA_CNN().to(DEVICE)
-            optimiser = torch.optim.Adam(model.parameters(), lr=parameter_config["LR"])
+            optimiser = torch.optim.AdamW(params=model.parameters(), lr=config["LR"], weight_decay=config["WEIGHT_DECAY"])
 
-            train_model(model=model, optimiser=optimiser, device=DEVICE, epochs=parameter_config["EPOCHS"], patience=parameter_config["PATIENCE"], dataloader=inner_train_loader, val_dataloader=inner_val_loader)
+            train_model(model=model, optimiser=optimiser, device=DEVICE, epochs=config["EPOCHS"], patience=config["PATIENCE"], dataloader=inner_train_loader, val_dataloader=inner_val_loader)
             val_loss = evaluate_model(model=model, device=DEVICE, dataloader=inner_val_loader)
             fold_losses.append(val_loss)
 
@@ -75,12 +79,12 @@ for i_outer_fold, (train_idx, test_idx) in enumerate(kf_outer.split(range(len(PA
 
         if mean_loss < best_inner_loss:
             best_inner_loss = mean_loss
-            best_model_config = parameter_config
+            best_model_config = config
     
     print(f"[OUTER {i_outer_fold+1}/{OUTER_K}] | ...", end="\r")
     outer_train_loader = DataLoader(SegmentDataset(outer_train_subjects), batch_size=best_model_config["BATCH_SIZE"], shuffle=True)
     model = OSA_CNN().to(DEVICE)
-    optimiser = torch.optim.Adam(model.parameters(), lr=best_model_config["LR"])
+    optimiser = torch.optim.AdamW(params=model.parameters(), lr=best_model_config["LR"], weight_decay=best_model_config["WEIGHT_DECAY"])
 
     train_model(model=model, optimiser=optimiser, device=DEVICE, epochs=best_model_config["EPOCHS"], patience=0, dataloader=outer_train_loader, val_dataloader=None)
 
@@ -92,6 +96,3 @@ for i_outer_fold, (train_idx, test_idx) in enumerate(kf_outer.split(range(len(PA
         "fold": i_outer_fold,
         **outer_test_metrics
     })
-
-# TEMPORARY RESULT DEBUG
-for result in results: print(result)
