19bf95bc0bb4618a695dc11ea76fd5a31a47e9f9
SpencerCooley07 <spencerjcooley07@gmail.com>
2025-07-18 17:00:22 +1000

    Completed NCV Test Loop + Comprehensive Terminal Logs

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from timeit import default_timer
 from torch import device, cuda
 from torch.utils.data import DataLoader
 from torch.optim import AdamW
-from scipy.stats import reciprocal
 from sklearn.model_selection import KFold, ParameterSampler
 
 from data import SubjectList, SegmentDataset
@@ -32,9 +31,11 @@ START_TIME = datetime.now()
 OUTPUT_PATH = path.join(path.abspath("output"), f"data-{START_TIME.strftime('%Y%m%d-%H%M%S')}")
 SUBJECT_LIST = SubjectList(path.abspath("data"))
 
-DEVICE = device("cuda" if cuda.is_available() else "cpu")
+# DEVICE = device("cuda" if cuda.is_available() else "cpu")
+DEVICE = "cuda" if cuda.is_available() else "cpu"
 OUTER_K, INNER_K = 5, 4
 RANDOM_STATE = 10
+TEST_BATCH_SIZE = 256
 
 ALPHA = 0.05 # > 0
 TARGET_PERCENTILE = 90 # < 100
@@ -89,7 +90,7 @@ HYPERPARAMETERS = {
         "GRID": {
             "LR": insert_logarithmic_means(start=1e-5, end=1e-4, n_means=3, is_int=False),
             "BATCH_SIZE": [128],
-            "EPOCHS": [5],
+            "EPOCHS": [1],
             "WEIGHT_DECAY": insert_logarithmic_means(start=1e-5, end=1e-3, n_means=3, is_int=False)
         }
     }
@@ -103,29 +104,26 @@ def ncv(inner_k: int, outer_k: int, network_type: str, hyperparameter_set: str, 
     network, hyperparameters = NETWORKS[network_type], HYPERPARAMETERS[hyperparameter_set]
 
     # === OVERVIEW HEADER ===
-    print(f"""┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
-┃ NCV TEST {START_TIME.strftime('[%Y-%m-%d] [%H:%M:%S]')}                                        ┃
-┃                                                                         ┃
-┃ TRAINING SETTINGS.                                                      ┃
-┃     OUTER FOLDS [{OUTER_K}]                                                     ┃
-┃     INNER FOLDS [{INNER_K}]                                                     ┃
-┃                                                                         ┃
-┃ RANDOM SEARCH SETTINGS                                                  ┃
-┃     CONFIDENCE [{int((1-ALPHA)*100):02d}%]                                                    ┃
-┃     PERCENTILE [{TARGET_PERCENTILE:02d}th]                                                   ┃
-┃     ITERATIONS [{hyperparameters['ITERATIONS']:02d}]                                                     ┃
-┃     COVERAGE [{round(hyperparameters['ITERATIONS'] / prod([len(options) for options in hyperparameters["GRID"].values()]) * 100):02d}%]                                                      ┃
-┃                                                                         ┃
-┃ MODEL CONFIGURATION [{network_type}]{' '*(50-len(network_type))}┃""")
+    print(f"""┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃ NCV TEST {START_TIME.strftime('[%Y-%m-%d] [%H:%M:%S]')}                                           ┃
+┃                                                                            ┃
+┃ TRAINING SETTINGS.                                                         ┃
+┃     OUTER FOLDS [{outer_k}]                                                        ┃
+┃     INNER FOLDS [{inner_k}]                                                        ┃
+┃                                                                            ┃
+┃ RANDOM SEARCH SETTINGS                                                     ┃
+┃     CONFIDENCE [{int((1-ALPHA)*100):02d}%]                                                       ┃
+┃     PERCENTILE [{TARGET_PERCENTILE:02d}th]                                                      ┃
+┃     ITERATIONS [{hyperparameters['ITERATIONS']:02d}]                                                        ┃
+┃     COVERAGE [{round(hyperparameters['ITERATIONS'] / prod([len(options) for options in hyperparameters["GRID"].values()]) * 100):02d}%]                                                         ┃
+┃                                                                            ┃
+┃ MODEL CONFIGURATION [{network_type}]{' '*(53-len(network_type))}┃""")
     
     for i, layer in enumerate(network):
-        if i == 0: print(f"┃     {layer['type'].upper()}{' '*(68-len(layer['type']))}┃")
-        else: print(f"┃     → {layer['type'].upper()}{' '*(66-len(layer['type']))}┃")
+        if i == 0: print(f"┃     {layer['type'].upper()}{' '*(71-len(layer['type']))}┃")
+        else: print(f"┃     → {layer['type'].upper()}{' '*(69-len(layer['type']))}┃")
 
-    print("""┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
-┏━━━━━━━┳━━━━━━━━━┳━━━━━━━┳━━━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━━━━┳━━━━━━━━━━━━┓
-┃ OUTER ┃ CONFIGS ┃ INNER ┃  ELAPSED  ┃  EPOCH  ┃    LOSS    ┃     F1     ┃
-┗━━━━━━━┻━━━━━━━━━┻━━━━━━━┻━━━━━━━━━━━┻━━━━━━━━━┻━━━━━━━━━━━━┻━━━━━━━━━━━━┛""")
+    print("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛")
     
 
 
@@ -134,15 +132,23 @@ def ncv(inner_k: int, outer_k: int, network_type: str, hyperparameter_set: str, 
     # Split data patient wise into K folds for outer CV loop
     outer_folds = KFold(n_splits=outer_k, shuffle=True, random_state=random_state)
     for i_outer, (i_outer_train, i_outer_test) in enumerate(outer_folds.split(subject_list.subjects)):
+        print(f"""┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃ OUTER [{(i_outer+1):02d}]                                                                 ┃
+┣━━━━━━━┳━━━━━━━━━┳━━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━┫
+┃ MODEL ┃ ELAPSED ┃   LOSS   ┃   ACC   ┃  PREC  ┃   TPR   ┃   TNR   ┃   F1   ┃
+┣━━━━━━━╇━━━━━━━━━╇━━━━━━━━━━╇━━━━━━━━━╇━━━━━━━━╇━━━━━━━━━╇━━━━━━━━━╇━━━━━━━━┫""")
+
         t_outer = default_timer()
 
         # Create list of custom SubjectData objects (in data/ecg_dataset.py)
         outer_train_set = [subject_list[i] for i in i_outer_train]
         outer_test_set = [subject_list[i] for i in i_outer_test]
 
-        # Tracking best hyperparameter settings
-        best_f1 = 0
-        best_config = None
+
+
+        # === HYPERPARAMETER RANDOM SEARCH ===
+
+        best_f1, i_best_config, best_config = 0, 0, None
 
         # Create hyperparameter configurations + inner fold split for inner cross validation
         inner_folds = KFold(n_splits=inner_k, shuffle=True, random_state=i_outer)
@@ -150,20 +156,58 @@ def ncv(inner_k: int, outer_k: int, network_type: str, hyperparameter_set: str, 
         for i_config, config in enumerate(configs):
             t_config = default_timer()
 
+            total_f1 = 0
+
+            # === INNER FOLDS ===
+
             for i_inner, (i_inner_train, i_inner_test) in enumerate(inner_folds.split(outer_train_set)):
+                print("┗━━━━━━━┷━━━━━━━━━┷━━━━━━━━━━┷━━━━━━━━━┷━━━━━━━━┷━━━━━━━━━┷━━━━━━━━━┷━━━━━━━━┛", end="\r")
                 t_inner = default_timer()
 
                 # Create inner DataLoaders for training/testing
-                inner_train_loader = DataLoader(dataset=SegmentDataset([subject_list[i] for i in i_inner_train]), batch_size=config["BATCH_SIZE"], shuffle=True)
-                inner_test_loader = DataLoader(dataset=SegmentDataset([subject_list[i] for i in i_inner_test]), batch_size=config["BATCH_SIZE"])
+                inner_train_loader = DataLoader(dataset=SegmentDataset([outer_train_set[i] for i in i_inner_train]), batch_size=config["BATCH_SIZE"], shuffle=True)
+                inner_test_loader = DataLoader(dataset=SegmentDataset([outer_train_set[i] for i in i_inner_test]), batch_size=TEST_BATCH_SIZE)
 
+                # Initialising Model + Optimiser
                 model = DynamicCNN(network)
                 optimiser = AdamW(params=model.parameters(), lr=config["LR"], weight_decay=config["WEIGHT_DECAY"])
 
+                # Training + Evaluating Model
                 train_model(model=model, optimiser=optimiser, device=DEVICE, epochs=config["EPOCHS"], dataloader=inner_train_loader)
                 performance = evaluate_model(model=model, device=DEVICE, dataloader=inner_test_loader)
+                metrics = performance["metrics"]
 
+                total_f1 += metrics['f1']
+                
+                print(f"""┃ {(i_config+1):02d}_{(i_inner+1):02d} │ {f"{(default_timer()-t_inner):.6f}"[:7]} │ {f"{performance['loss']:.7f}"[:8]} │ {f"{metrics['accuracy']*100:.6f}"[:7]} │ {f"{metrics['precision']:.5f}"[:6]} │ {f"{metrics['recall']:.6f}"[:7]} │ {f"{metrics['specificity']:.6f}"[:7]} │ {f"{metrics['f1']:.5f}"[:6]} ┃""")
+                if (i_inner + 1 == inner_k) and (i_config + 1 != hyperparameters["ITERATIONS"]): print("┣━━━━━━━┿━━━━━━━━━┿━━━━━━━━━━┿━━━━━━━━━┿━━━━━━━━┿━━━━━━━━━┿━━━━━━━━━┿━━━━━━━━┫")
+                elif (i_config + 1 != hyperparameters["ITERATIONS"]) or (i_inner + 1 != inner_k): print("┠───────┼─────────┼──────────┼─────────┼────────┼─────────┼─────────┼────────┨")
 
+            if total_f1 > best_f1:
+                best_f1 = total_f1
+                best_config = config
+                i_best_config = i_config
+
+        print("┣━━━━━━━┷━━━━━━━━━┷━━━━━━━━━━┷━━━━━━━━━┷━━━━━━━━┷━━━━━━━━━┷━━━━━━━━━┷━━━━━━━━┫")
+        print("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛", end="\r")
+
+        t_test = default_timer()
+
+        # Create outer DataLoaders for training/testing
+        outer_train_loader = DataLoader(dataset=SegmentDataset(outer_train_set), batch_size=best_config["BATCH_SIZE"], shuffle=True)
+        outer_test_loader = DataLoader(dataset=SegmentDataset(outer_test_set), batch_size=TEST_BATCH_SIZE)
+
+        model = DynamicCNN(network)
+        optimiser = AdamW(params=model.parameters(), lr=best_config["LR"], weight_decay=best_config["WEIGHT_DECAY"])
+        
+        train_model(model=model, optimiser=optimiser, device=DEVICE, epochs=best_config["EPOCHS"], dataloader=outer_train_loader)
+        performance = evaluate_model(model=model, device=DEVICE, dataloader=outer_test_loader)
+        metrics = performance["metrics"]
+
+        print(f"""┃ OUTER [{(i_outer+1):02d}] TOTAL TIME: {f"{(default_timer()-t_outer):.6f}"[:7]}s                                            ┃
+┠───────┬─────────┬──────────┬─────────┬────────┬─────────┬─────────┬────────┨
+┃ {(i_outer+1):02d}_{(i_best_config+1):02d} │ {f"{(default_timer()-t_test):.6f}"[:7]} │ {f"{performance['loss']:.7f}"[:8]} │ {f"{metrics['accuracy']*100:.6f}"[:7]} │ {f"{metrics['precision']:.5f}"[:6]} │ {f"{metrics['recall']:.6f}"[:7]} │ {f"{metrics['specificity']:.6f}"[:7]} │ {f"{metrics['f1']:.5f}"[:6]} ┃""")
+        print("┗━━━━━━━┷━━━━━━━━━┷━━━━━━━━━━┷━━━━━━━━━┷━━━━━━━━┷━━━━━━━━━┷━━━━━━━━━┷━━━━━━━━┛")
 
         # FOR JSON SAVING
         # mkdir(output_path)
@@ -195,5 +239,5 @@ def standard(network_type: str, lr: float, batch_size: int, epochs: int, weight_
 
 
 if __name__ == "__main__":
-    standard(network_type="MAIN", lr=1e-5, batch_size=64, epochs=50, weight_decay=1e-5, train_proportion=0.8, subject_list=SUBJECT_LIST)
-    # ncv(inner_k=INNER_K, outer_k=OUTER_K, network_type="MAIN", hyperparameter_set="MAIN", subject_list=SUBJECT_LIST, output_path=OUTPUT_PATH, random_state=RANDOM_STATE)
+    # standard(network_type="MAIN", lr=1e-5, batch_size=64, epochs=50, weight_decay=1e-5, train_proportion=0.8, subject_list=SUBJECT_LIST)
+    ncv(inner_k=INNER_K, outer_k=OUTER_K, network_type="DEBUG", hyperparameter_set="DEBUG", subject_list=SUBJECT_LIST, output_path=OUTPUT_PATH, random_state=RANDOM_STATE)
