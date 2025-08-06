c4a05d7ed45500e0e545aaeb794e00d488ca6e4f
SpencerCooley07 <spencerjcooley07@gmail.com>
2025-08-06 22:30:31 +1000

    Fix os.listdir Arbitrary Order for UNIX Systems

--- data/ecg_dataset.py
+++ data/ecg_dataset.py
@@ -17,7 +17,9 @@ class SubjectData:
 class SubjectList:
     def __init__(self, data_dir: str):
         self.subjects = []
-        for file_path in os.listdir(data_dir):
+        npz_files = sorted([f for f in os.listdir(data_dir) if f.endswith('.npz')])
+        for file_path in npz_files:
+            print(file_path)
             if not file_path.endswith('.npz'): continue
             data = np.load(os.path.join(data_dir, file_path))
             self.subjects.append(SubjectData(segments=data['segments'], labels=data['labels']))
